3ed535e13b6044751f1dbd0577cd309f563a5480
Gunaya <gunnayaa@gmail.com>
2019-05-06 15:42:24 +0800

    change logic cleaning locations

--- cleaning_tweet.py
+++ cleaning_tweet.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 import json
+import re
 
 MONGO_HOST = 'mongodb://localhost/'
 MONGO_DB = 'testing'
+FLAG = 0;
 
 def get_json(collection_name):
     client = MongoClient(MONGO_HOST)
@@ -12,69 +14,149 @@ def get_json(collection_name):
 
 def save_json(collection_name):
     client = MongoClient(MONGO_HOST)
-    db = client["clean_tweet"]
+    db = client["hasil"]
     collection = db[collection_name]
     return collection
 
 def main(collection_name):
     # get json data from MongoDB
+
     data = get_json(collection_name)
     data_details = data.find()
     col_name = collection_name.replace("#",'')
     new_collection = save_json(col_name)
 
     tweet_count = 1
+    none_count = 0
     for i in data_details:
 
         locations = i['location']
         locations = locations.split(',')
 
         for x in range(len(locations)):
-            locations[x] = locations[x].replace(' ', '')
-            locations[x] = location_filter(locations[x])
+            locations[x] = locations[x].strip()
+            locations[x] = locations[x].replace('.','')
+            if locations[x].isalpha() == True:
+                locations[x] = location_filter(locations[x])
+            else:
+                locations[x] = ''
+
             if locations[x] == '':
                 locations[x] = 'none'
+                none_count += 1
 
         new_location = 'none'
         for x in range(len(locations)):
             if locations[x] != 'none':
-                new_location = locations[x]
+                if new_location == 'none':
+                    new_location = locations[x]
+                else:
+                    if FLAG == 1:
+                        if new_location != locations[x]:
+                            new_location = locations[x]
+                    elif FLAG == 0:
+                        new_location = new_location
 
-        tweet_count += 1
-
+        # save JSON data into new Collection on MongoDB
         data = {}
         data['tweet_id'] = i['id']
         data['tweet_location'] = new_location
         data['created_at'] = i['created_at']
 
-        new_collection.insert(data)
+        # new_collection.insert(data)
 
-        print("{} tweet successfully imported into {} collection on MongoDB".format(tweet_count, col_name))
+        # print("{} tweet successfully imported into {} collection on MongoDB".format(tweet_count, col_name))
+
+        print(tweet_count)
+        print(new_location)
+
+        tweet_count += 1
+
+    print('None : {}'.format(none_count))
 
 def location_filter(kota):
     with open('countries.json') as f:
         data = json.load(f)
-    # kota = 'Visakhapatnam'
+
+    # Inisialisasi Singkatan
+    US = ['usa', 'us', 'america', 'fl', 'nyc', 'dc', 'oh', 'tx', 'ma', 'mn']
     country = ''
+
+    # filepath = 'City Lists/CITIES2.TXT'
+    # with open(filepath) as fp:
+    #     for cnt, line in enumerate(fp):
+    #         line = line.strip().split(None, 1)
+    #         if not len(line) == 1:
+    #             x = line[1]
+    #             if any(re.findall('|'.join(US), kota.lower())):
+    #                 country = 'United States'
+    #                 break
+    #             elif kota.lower() == 'bc':
+    #                 country = 'Canada'
+    #                 break
+    #             elif kota.lower() == 'uk' or kota.lower() == 'u.k':
+    #                 country = 'United Kingdom'
+    #                 break
+    #             elif kota.lower() == x.lower():
+    #                 country = line[0]
+    #                 print("found")
+    #                 break
+    #             else:
+    #                 country = kota
+    #                 break
+
     for countries in data:
         if kota.lower() == countries.lower():
             country = countries
         else:
             for x in data[countries]:
-                if kota.lower() == 'usa' or kota.lower() == 'u.s.a' or kota.lower() == 'us' or kota.lower() == 'nyc':
-                    country = 'United States'
-                    break
-                elif kota.lower() == 'uk' or kota.lower() == 'u.k':
-                    country = 'United Kingdom'
-                    break
-                elif kota.lower() == x.lower():
+                if kota.lower() == x.lower():
+                    FLAG = 1;
+                    print('found')
                     country = countries
                     break
+                elif any(re.findall('|'.join(US), kota.lower())):
+                    FLAG = 1;
+                    country = 'United States'
+                    break
+                elif kota.lower() == 'bc':
+                    FLAG = 1;
+                    country = 'Canada'
+                    break
+                elif kota.lower() == 'uk' or kota.lower() == 'u.k' or kota.lower() == 'england':
+                    FLAG = 1;
+                    country = 'United Kingdom'
+                    break
+                elif kota.lower() != x.lower():
+                    FLAG = 0;
+                    country = kota
+                    break
+                # elif kota.lower() != '' and kota.lower() != x.lower():
+                #     country = kota.lower()
+                #     print("tessse")
+                #     break
     return country
 
 if __name__ == "__main__":
-    query = ['chikungunya', 'dengue', 'ebola', 'hiv', 'malaria', 'measles', 'mers', 'polio', 'rabies', 'tuberculosis', 'yellowfever', 'zika']
+    # query = ['chikungunya', 'dengue', 'ebola', 'hiv', 'malaria', 'measles', 'mers', 'polio', 'rabies', 'tuberculosis', 'yellowfever', 'zika']
 
-    for q in query:
-        main(q)
+    # for q in query:
+    #     main(q)
     # location_filter()
+
+    main('yellowfever')
+
+    # with open('countries.json') as f:
+    #     data = json.load(f)
+    # kota = 'London'
+    # for countries in data:
+    #     if kota.lower() == countries.lower():
+    #         country = countries
+    #     else:
+    #         for x in data[countries]:
+    #             if kota.lower() == x.lower():
+    #                 FLAG = 1;
+    #                 print('found')
+    #                 country = countries
+    #                 print(countries)
+    #                 break
--- tweet_mining.py
+++ tweet_mining.py
@@ -45,7 +45,7 @@ def get_tweet(db, collection_name, filepath, api, query, max_tweets):
     tweetCount = 0
 
     with open("./json/"+filepath, 'a+') as f:
-        for tweet in limit_handled(tweepy.Cursor(api.search, q=query, since="2019-4-10", until="2019-4-11", tweet_mode='extended').items(max_tweets)):
+        for tweet in limit_handled(tweepy.Cursor(api.search, q=query, since="2019-4-30", until="2019-5-1", tweet_mode='extended').items(max_tweets)):
             if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
                 f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
                 #json.dump(tweet._json, f, indent=2)
